ad8f59cf644134f569284a72d5709d813575c750
Melody Nguyen <melodyxnguyen@gmail.com>
2025-02-14 13:43:45 -0500

    React practice file

---
+++ PracticeCode.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {TouchableHighlight, Text} from 'react-native'
+
+const App = () => {
+    return (
+        <TouchableHighlight>
+            <Text>
+                Hello World!</Text>
+        </TouchableHighlight>
+    )
+}
